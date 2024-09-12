47ff8b2c4fd735bc5763acaa732cacdf74cfa95b
Derrick <62232342+Dranol@users.noreply.github.com>
2024-09-12 13:01:22 +0000

    2nd day commits

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 @app.route("/")
 def hello():
-    return "Hello Worldies"
+    return  render_template('indexPage.html')
 
 if __name__ == "__main__":
   app.run(host="0.0.0.0", debug=True)
